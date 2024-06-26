22d8fdcea425dd0cc7b666e7f0eacf24da0ccb61
Sterphane Roque <stermiss2@gmail.com>
2024-06-25 22:41:38 -0300

    chore: app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Modal } from "./components/Modal";
+
 export default function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="w-screen h-screen bg-black flex items-center justify-center">
+      <Modal />
+    </div>
+  );
 }
