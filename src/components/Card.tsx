106c8900875831f790ab03d21265881fc37d9fa6
Sterphane Roque <stermiss2@gmail.com>
2024-06-26 15:28:21 -0300

    chore: add progress bar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,42 @@
+import { useProgress } from "../hooks/useProgress";
+import { ProgressBar } from "./ProgressBar";
+
 type CardProps = {
   character: string;
   activeIndex: number;
 };
 
 export function Card({ character, activeIndex }: CardProps) {
-  return (
-    <div className="flex gap-3">
-      {character.split("").map((digit, index) => {
-        const isActive = index <= activeIndex - 1;
+  const { progress } = useProgress();
 
-        return (
-          <div
-            key={index}
-            className={`w-12 h-12 text-center rounded-md font-bold text-xl items-center justify-center flex text-white border-2 border-solid border-gray-300 ${
-              isActive
-                ? "border-2 border-yellow-200 border-solid bg-yellow-500 text-black"
-                : "bg-blue-900"
-            }`}
-          >
-            {digit}
-          </div>
-        );
-      })}
-    </div>
+  return (
+    <>
+      <div>
+        <p className="text-white font-bold text-center mb-2">
+          Pressione as teclas na ordem correta
+        </p>
+        <div className={`flex gap-3`}>
+          {character.split("").map((digit, index) => {
+            const isActive = index <= activeIndex - 1;
+
+            return (
+              <div
+                key={index}
+                className={`w-12 h-12 text-center rounded-md font-bold text-xl items-center justify-center flex text-white border-2 border-solid border-gray-300 ${
+                  isActive
+                    ? "border-2 border-yellow-200 border-solid bg-yellow-500 text-black"
+                    : "bg-blue-900"
+                }
+                  `}
+              >
+                {digit}
+              </div>
+            );
+          })}
+        </div>
+        {progress}
+        <ProgressBar progress={progress} />
+      </div>
+    </>
   );
 }
